4d6f4a07dae192a0e5e4b6ba25f7f2ebc3ab67f7
lnchen6 <lnchen6@iflytek.com>
2022-06-29 17:46:07 +0800

    [修改代码]修改传参形式

--- upload-front/src/tool/api.ts
+++ upload-front/src/tool/api.ts
@@ -3,7 +3,12 @@ import { AxiosRequestConfig } from 'axios'
 import { resBase, reqBase64 } from './type'
 
 export function UploadSingle(param: FormData, other?: AxiosRequestConfig): Promise<resBase> {
-  return request.post('/upload_single', param, other)
+  const config =Object.assign({
+    headers: {
+        'Content-Type': 'multipart/form-data',
+    },
+  },other)
+  return request.post('/upload_single', param,config)
 }
 export function UploadBase64(param: reqBase64): Promise<resBase> {
   return request.post('/upload_single_base64', param)
@@ -12,7 +17,11 @@ export function uploadBigCheck(md5Val: string): Promise<resBase> {
   return request.post('/bigFile/check', { md5Val })
 }
 export function uploadBigUp(md5Val: string, param: FormData): Promise<resBase> {
-  return request.post(`/bigFile/upload?md5Val=${md5Val}`, param)
+  return request.post(`/bigFile/upload?md5Val=${md5Val}`, param,{
+    headers: {
+        'Content-Type': 'multipart/form-data',
+    },
+  })
 }
 export function uploadBigMerge(md5Val: string, ext: string): Promise<resBase> {
   return request.post('/bigFile/merge', { md5Val, ext })
--- upload-front/src/tool/request.ts
+++ upload-front/src/tool/request.ts
@@ -2,19 +2,19 @@
  * axios 请求的公共信息进行提取
  */
 // import axios  from 'axios'
-import axios from 'axios'
-// import qs from 'qs'
+import axios ,{AxiosRequestHeaders}from 'axios'
+import qs from 'qs'
 const instance = axios.create();
 instance.defaults.baseURL = 'http://127.0.0.1:8880'
 
-//  instance.defaults.transformRequest = (data, headers) => {
-//      const contentType = (headers as AxiosRequestHeaders)['Content-Type']
+ instance.defaults.transformRequest = (data, headers) => {
+     const contentType = (headers as AxiosRequestHeaders)['Content-Type']
 
-//      if (contentType === 'application/x-www-form-urlencoded') {
-//          return qs.stringify(data) // 传递参数格式
-//      }
-//      return data    
-//  }
+     if (contentType === 'multipart/form-data') {
+         return data
+     }
+     return qs.stringify(data) // 传递参数格式  
+ }
 // 响应拦截器
 instance.interceptors.response.use(response => {
     return response.data
